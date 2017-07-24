d0a9ffb39cee2bd9357bf07f9d849e4e0e1a0731
coldlook <dream.ct@qq.com>
2017-07-25 00:00:10 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,8 +6,30 @@ use think\Db;
 
 class Index extends HomeBase
 {
-    public function index()
+    public function index($cid=1, $page = 1)
     {
-        return $this->fetch();
+        $map   = array();		
+        $category_model = new CategoryModel();
+        $category_children_ids = $category_model->where(['path' => ['like', "%,{$cid},%"]])->column('id');
+        $category_children_ids = (!empty($category_children_ids) && is_array($category_children_ids)) ? implode(',', $category_children_ids) . ',' . $cid : $cid;
+        $map['cid']            = ['IN', $category_children_ids];
+        $articleModel=new ArticleModel();
+    	$lists = $articleModel->where($map)->order('is_top,sort,publish_time DESC')->paginate(10);
+
+        $category= CategoryModel::get($cid);
+        if ($category['pid'] == 0) {
+            $category_list= $category_model->where(['pid' => $cid])->select();
+            $child_cid=0;
+        }else{
+            $category_list= $category_model->where(['pid' => $category['pid']])->select();
+            $child_cid=$cid;
+            $cid=$category['pid'];
+        }	
+        //热门点击
+        $map11['status']  = 1;
+        $map11['is_recommend']  = 1;
+        $map11['label']  = ['like','%2%'];
+        $listReading=Db::name('article')->where($map11)->order('sort DESC')->limit(6) ->select();  
+		return $this->fetch('index',['lists' => $lists,'current_nav'=>'/lists/'.$cid,'category'=>$category,'category_list'=>$category_list,'child_cid'=>$child_cid,'reading'=>$listReading,]);
     }
 }
--- public/home/js/common.js
+++ public/home/js/common.js
@@ -9,3 +9,17 @@ function shake(o, end) {
     var shakeTimer = setTimeout(function() { shake(o, end) }, timeout);
     o[end] = function() { clearTimeout(shakeTimer) };
 }
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('#back-to-top').fadeIn();
+    } else {
+        $('#back-to-top').fadeOut();
+    }
+});
+$('#back-to-top').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+    return false;
+});
